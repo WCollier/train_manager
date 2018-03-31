3a3ab5c6b44d4001cc205df3f7358181a3416277
Willifme <me@williamcollier.me.uk>
2018-03-31 19:15:01 +0100

    Index view now filters by user

--- manager/views.py
+++ manager/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Collection, ModelTrain
 
 class IndexView(generic.ListView):
+    model = Collection
+
     template_name = 'manager/index.html'
 
     context_object_name = 'collection_list'
 
     def get_queryset(self):
-        """Return all the models (for now)"""
-        return Collection.objects.all()
+        if self.request.user.is_authenticated:
+            #return Collection.objects.filter(owner=self.request.user)
+            return Collection.objects.all()
+
+        else:
+            return Collection.objects.none()
