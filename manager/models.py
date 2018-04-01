ac07d165e16c5beb8bf792f427399ea74c8c95fc
Willifme <me@williamcollier.me.uk>
2018-04-01 18:04:05 +0100

    Change " to '. Add get_absolute_url

--- manager/models.py
+++ manager/models.py
@@ -1,35 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 from . import manufacturers
 
 class ModelTrain(models.Model):
     TRACTION_TYPES = (
-        ("Diesel", "Diesel"),
-        ("Petrol", "Petrol"),
-        ("Steam", "Steam"),
-        ("Electric", "Electric"),
+        ('Diesel', 'Diesel'),
+        ('Petrol', 'Petrol'),
+        ('Steam', 'Steam'),
+        ('Electric', 'Electric'),
     )
 
     SCALES = (
-        ("OO", "OO"),
-        ("HO", "HO"),
-        ("TT", "TT"),
-        ("N", "N"),
+        ('OO', 'OO'),
+        ('HO', 'HO'),
+        ('TT', 'TT'),
+        ('N', 'N'),
     )
-
-    manufacturer = models.CharField(max_length=100, choices=manufacturers.MANUFACTURERS)
-
     name = models.CharField(max_length=30)
+
+    manufacturer = models.CharField(max_length=100, choices=manufacturers.MANUFACTURERS, default=manufacturers.MANUFACTURERS[0][0])
 
     model_class = models.CharField(max_length=30)
 
-    traction = models.CharField(max_length=10, choices=TRACTION_TYPES)
+    traction = models.CharField(max_length=10, choices=TRACTION_TYPES, default=TRACTION_TYPES[0][0])
 
-    scale = models.CharField(max_length=2, choices=SCALES)
+    scale = models.CharField(max_length=2, choices=SCALES, default=SCALES[0][0])
 
     era = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(9)])
+
+    def get_absolute_url(self):
+        return reverse('model-train-detail', args=[str(self.id)])
 
     def __str__(self):
         return self.name
