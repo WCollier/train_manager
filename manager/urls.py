851d3b24dd9595c4a77c11d6a006eeac6f7b77b3
Willifme <me@williamcollier.me.uk>
2018-04-01 18:04:53 +0100

    Add models cread, read, update and delete operations.

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('models/', views.ModelTrains.as_view(), name='model-trains'),
+    path('models/add/', views.ModelTrainCreate.as_view(), name='model-train-add'),
+    path('models/<slug:pk>/', views.ModelTrainDetail.as_view(), name='model-train-detail'),
+    path('models/<int:pk>/update/', views.ModelTrainUpdate.as_view(), name='model-train-update'),
+    path('models/<int:pk>/delete/', views.ModelTrainDelete.as_view(), name='model-train-delete'),
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,8 +1,11 @@
-from django.views import generic
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .models import Collection, ModelTrain
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     model = Collection
 
     template_name = 'manager/index.html'
@@ -15,3 +18,56 @@ class IndexView(generic.ListView):
 
         else:
             return Collection.objects.none()
+
+class ModelTrains(LoginRequiredMixin, ListView):
+    model = ModelTrain
+
+    template_name = 'manager/modeltrains.html'
+
+    context_object_name = 'model_trains'
+
+    login_url = '/login/'
+
+    redirect_field_name = 'redirect_to'
+
+    def get_queryset(self):
+        return ModelTrain.objects.all()
+
+class ModelTrainDetail(LoginRequiredMixin, DetailView):
+    model = ModelTrain
+
+    login_url = '/login/'
+
+    redirect_field_name = 'redirect_to'
+
+    fields = ['name', 'manufacturer', 'model_class', 'traction', 'scale', 'era']
+
+class ModelTrainCreate(LoginRequiredMixin, CreateView):
+    model = ModelTrain
+
+    login_url = '/login/'
+
+    redirect_field_name = 'redirect_to'
+
+    fields = ['name', 'manufacturer', 'model_class', 'traction', 'scale', 'era']
+
+class ModelTrainUpdate(LoginRequiredMixin, UpdateView):
+    model = ModelTrain
+
+    login_url = '/login/'
+
+    redirect_field_name = 'redirect_to'
+
+    fields = ['name', 'manufacturer', 'model_class', 'traction', 'scale', 'era']
+
+    def get_success_url(self):
+        return reverse_lazy('model-trains')
+
+class ModelTrainDelete(LoginRequiredMixin, DeleteView):
+    model = ModelTrain
+
+    success_url = reverse_lazy('index')
+
+    login_url = '/login/'
+
+    redirect_field_name = 'redirect_to'
