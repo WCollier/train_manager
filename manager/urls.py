dd7ffee733c93b2b330d6a4f0a6d4c23ac304268
Willifme <me@williamcollier.me.uk>
2018-04-03 20:44:45 +0100

    Add delete and update for collections

--- manager/urls.py
+++ manager/urls.py
@@ -4,10 +4,15 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+
     path('collections/', views.Collections.as_view(), name='collections'),
+    path('collections/new/', views.CollectionCreate.as_view(), name='collection-create'),
     path('collections/<slug:pk>/', views.CollectionsDetail.as_view(), name='collections-detail'),
+    path('collections/<int:pk>/update/', views.CollectionsUpdate.as_view(), name='collections-update'),
+    path('collections/<int:pk>/delete/', views.CollectionsDelete.as_view(), name='collections-delete'),
+
     path('models/', views.ModelTrains.as_view(), name='model-trains'),
-    path('models/add/', views.ModelTrainCreate.as_view(), name='model-train-add'),
+    path('models/new/', views.ModelTrainCreate.as_view(), name='model-train-create'),
     path('models/<slug:pk>/', views.ModelTrainDetail.as_view(), name='model-train-detail'),
     path('models/<int:pk>/update/', views.ModelTrainUpdate.as_view(), name='model-train-update'),
     path('models/<int:pk>/delete/', views.ModelTrainDelete.as_view(), name='model-train-delete'),
